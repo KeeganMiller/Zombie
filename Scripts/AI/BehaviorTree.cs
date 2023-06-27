e45698e45d4ef91c310b057811e5d5cf64dfe469
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-27 13:30:16 +1000

    Added the root task and an abstract method to create the tree

--- Scripts/AI/BehaviorTree.cs
+++ Scripts/AI/BehaviorTree.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using Godot;
 
-public class BehaviorTree
+public abstract class BehaviorTree
 {
     protected Blackboard _BlackboardRef;
     public Blackboard BlackboardRef => _BlackboardRef;
 
     public bool Paused = false;
+    protected Task _RootTask;
 
     public BehaviorTree(Blackboard blackboard)
     {
         _BlackboardRef = blackboard;
+        _RootTask = CreateTree();
     }
 
     public void Update(float delta)
@@ -19,4 +21,6 @@ public class BehaviorTree
         if (Paused || _BlackboardRef == null)
             return;
     }
+
+    protected abstract Task CreateTree();
 }
