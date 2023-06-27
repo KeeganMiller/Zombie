77b68fc504955d95f86c573cbcc456a8367c6aa2
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-27 12:38:51 +1000

    Crate the character details

---
+++ Scripts/Character/CharacterDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Godot;
+
+[Serializable]
+public class CharacterDetails
+{
+    private string _FirstName;
+    private string _LastName;
+    private float _Age;
+
+    public string FirstName => _FirstName;
+    public string LastName => _LastName;
+    public float Age => _Age;
+}
