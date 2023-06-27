2da21b80c6d04feeb571b6b462d4bab101e17191
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-27 12:38:59 +1000

    Initial character controller

---
+++ Scripts/Character/BaseCharacterController.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class BaseCharacterController : CharacterBody2D
+{
+	// === CHARACTER DETAILS === //
+	
+	
+	public override void _PhysicsProcess(double delta)
+	{
+		
+	}
+}
