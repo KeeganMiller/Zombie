8524a03694737151d301e488de52c698d9668baf
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-27 14:11:14 +1000

    Intiial move to locastion task

--- Scripts/AI/Task/MoveToLocationTask.cs
+++ Scripts/AI/Task/MoveToLocationTask.cs
@@ -1,6 +1,33 @@
-﻿namespace Zombie.Scripts.AI.Task;
+﻿using System;
+using System.Collections.Generic;
+using Godot;
 
-public class MoveToLocationTask
+public class MoveToLocationTask : Task
 {
-    
+    private const float STOPPING_DISTANCE = 0.5f;
+    public MoveToLocationTask(BehaviorTree tree) : base(tree)
+    {
+    }
+
+    public MoveToLocationTask(BehaviorTree tree, List<Task> children) : base(tree, children)
+    {
+    }
+
+    public override ETaskState RunTask(float delta)
+    {
+        if (_Tree == null)
+            return ETaskState.FAILURE;
+
+        Blackboard bb = _Tree.BlackboardRef;
+        if (bb != null)
+        {
+            Node2D Self = bb.GetValueAsNode("Self");
+            if (Self != null)
+            {
+                
+            }
+        }
+
+        return ETaskState.FAILURE;
+    }
 }
