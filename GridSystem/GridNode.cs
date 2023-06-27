3df1f3514fed2e73b3ca1ec5b871f9a32f7c9af8
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-27 12:08:05 +1000

    Setup the grid node class with default values

--- GridSystem/GridNode.cs
+++ GridSystem/GridNode.cs
@@ -3,5 +3,15 @@ using System;
 
 public class GridNode
 {
-	
+	private GridController _GridRef;					// Reference to the grid
+	private Vector2 _CellPosition;
+	private int _CellIndexX;
+	private int _CellIndexY;
+	public GridNode(GridController grid, Vector2 gridPosition, int cellX, int cellY)
+	{
+		_GridRef = grid;
+		_CellPosition = gridPosition;
+		_CellIndexX = cellX;
+		_CellIndexY = cellY;
+	}
 }
