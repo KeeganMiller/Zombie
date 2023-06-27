a41a70bd6efe335e9abb32ec76687ede32004ff6
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-28 02:01:29 +1000

    Create check to see if we have a move to location

---
+++ Scripts/AI/Checks/MoveToLocationCheck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Godot;
+
+public class MoveToLocationCheck : Task
+{
+    public MoveToLocationCheck(BehaviorTree tree) : base(tree)
+    {
+    }
+
+    public MoveToLocationCheck(BehaviorTree tree, List<Task> children) : base(tree, children)
+    {
+    }
+
+    public override ETaskState RunTask(float delta)
+    {
+        if (_Tree == null)
+            return ETaskState.FAILURE;
+
+        Blackboard bb = _Tree.BlackboardRef;
+        if (bb != null)
+        {
+            if (bb.GetValueAsBool("HasMoveToLocation") && bb.GetValueAsVector2("MoveToLocation") != Vector2.Zero)
+                return ETaskState.RUNNING;
+        }
+
+        return ETaskState.FAILURE;
+    }
+}
