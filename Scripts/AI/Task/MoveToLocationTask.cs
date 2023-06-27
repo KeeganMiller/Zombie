27a770d127c481c946b2587f1ce63439ca100963
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-28 02:01:19 +1000

    Create the method to run the task

--- Scripts/AI/Task/MoveToLocationTask.cs
+++ Scripts/AI/Task/MoveToLocationTask.cs
@@ -18,13 +18,35 @@ public class MoveToLocationTask : Task
         if (_Tree == null)
             return ETaskState.FAILURE;
 
-        Blackboard bb = _Tree.BlackboardRef;
+        Blackboard bb = _Tree.BlackboardRef;                // Get reference to the blackboard
         if (bb != null)
         {
-            Node2D Self = bb.GetValueAsNode("Self");
-            if (Self != null)
+            Node2D self = bb.GetValueAsNode("Self");            // Get self reference
+            if (self != null)
             {
+                Vector2 currentPos = self.GlobalPosition;                   // Get the agents current position
+                Vector2 moveToPos = bb.GetValueAsVector2("MoveToLocation");         // Get the position we wish to move to
                 
+                // Check if we are in stopping distance of the location, if not than set the location
+                // Otherwise we have reached our location
+                if (currentPos.DistanceTo(moveToPos) > STOPPING_DISTANCE)
+                {
+                    if (self is BaseCharacterController character)
+                    {
+                        character.Agent?.SetTargetLocation(moveToPos);
+                        return ETaskState.RUNNING;
+                    }
+                    else
+                    {
+                        GD.PrintErr("#MoveToLocation-Task: Failed to cast to character controller");
+                    }
+                }
+                else
+                {
+                    bb.SetValueAsBool("HasMoveToLocation", false);
+                    bb.SetValueAsVector2("MoveToLocation", Vector2.Zero);
+                    return ETaskState.SUCCESS;
+                }
             }
         }
 
