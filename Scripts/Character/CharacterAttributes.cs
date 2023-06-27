6029bd1150b9c0f82179d8bce4f4fb477804ef0b
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-27 12:33:57 +1000

    Added properties with getters

--- Scripts/Character/CharacterAttributes.cs
+++ Scripts/Character/CharacterAttributes.cs
@@ -5,5 +5,21 @@ using Godot;
 [Serializable]
 public class CharacterAttributes
 {
+    private const int MAX_LEVEL = 10;
+
+    // === PROOPERTIES === //
+    private int _Strength;
+    private int _Dexterity;
+    private int _Vitality;
+    private int _Intelligence;
+    private int _Charisma;
     
+    // === GETTERS === //
+    public int Strength => _Strength;
+    public int Dexterity => _Dexterity;
+    public int Vitality => _Vitality;
+    public int Intelligence => _Intelligence;
+    public int Charisma => _Charisma;
+
+
 }
