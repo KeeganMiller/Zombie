107c436e9a9a856b57de5eccbd87596eb2021916
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-27 14:11:53 +1000

    Added pathfinding properties

--- Scripts/GridSystem/GridNode.cs
+++ Scripts/GridSystem/GridNode.cs
@@ -1,13 +1,26 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class GridNode
 {
 	private GridController _GridRef;					// Reference to the grid
-	private Vector2 _CellPosition;
+	private Vector2 _CellPosition;						// Reference to the cell position
 	public Vector2 CellPosition => _CellPosition;
-	private int _CellIndexX;
-	private int _CellIndexY;
+	private int _CellIndexX;							// Reference to the cell X index
+	public int CellIndexX => _CellIndexX;
+	private int _CellIndexY;							// Reference to the cell Y index
+	public int CellIndexY => _CellIndexY;
+	
+	
+	
+	// === PATHFINDING === //
+	public bool IsWalkable = true;					// If this node can be walked on
+	public int GCost;
+	public int HCost;
+	public int FCost => GCost + HCost;
+	public List<GridNode> _Neighbors = new List<GridNode>();
+	
 	public GridNode(GridController grid, Vector2 gridPosition, int cellX, int cellY)
 	{
 		_GridRef = grid;
