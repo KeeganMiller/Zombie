cb9643d8d7dc4cbc278e2406c703a86dd6c70be9
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-28 02:01:00 +1000

    Created the inital settler tree

---
+++ Scripts/AI/Trees/SettlerTree.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Godot;
+
+public class SettlerTree : BehaviorTree
+{
+    public SettlerTree(Blackboard blackboard) : base(blackboard)
+    {
+    }
+
+    protected override Task CreateTree()
+    {
+        return new Selector(this, new List<Task>
+        {
+            new Sequence(this, new List<Task>
+            {
+                new MoveToLocationCheck(this),
+                new MoveToLocationTask(this)
+            })
+        });
+    }
+}
