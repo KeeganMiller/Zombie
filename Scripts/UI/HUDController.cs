a7e6f4cd45b24bfc7acaf25157118aad255f3106
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-29 08:05:03 +1000

    Created initial HUD controller and method to toggle between build and play mode

---
+++ Scripts/UI/HUDController.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+public partial class HUDController : CanvasLayer
+{
+	[ExportGroup("Toggle Mode Buttons")]
+	[Export] private Texture2D _PlayTexture;
+	[Export] private Texture2D _BuildTexture;
+	[Export] private NodePath _TogglePath;
+
+	private TextureButton _ToggleButton;
+
+	public override void _Ready()
+	{
+		base._Ready();
+		_ToggleButton = GetNode<TextureButton>(_TogglePath);
+	}
+	public void OnModeTogglePressed()
+	{
+		BuildModeController buildMode = GetNode<BuildModeController>("/root/BuildModeController");
+		if (buildMode != null)
+		{
+			if (buildMode.IsBuildMode)
+			{
+				buildMode.IsBuildMode = false;
+				if (_ToggleButton != null)
+					_ToggleButton.TextureNormal = _BuildTexture;
+			}
+			else
+			{
+				buildMode.IsBuildMode = true;
+				if (_ToggleButton != null)
+					_ToggleButton.TextureNormal = _PlayTexture;
+			}
+		}
+	}
+}
