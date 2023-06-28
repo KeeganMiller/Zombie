f8ff8dda770c95162191289a1b5e9ce3bdccc64a
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-29 08:04:37 +1000

    Created a controller to handle placing a wall

---
+++ Scripts/Building/WallController.cs
@@ -0,0 +1,68 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class WallController : Node2D
+{
+    private List<Sprite2D> _WallObjects = new List<Sprite2D>();
+    public int _WallObjectIndex = 0;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        
+        // Store all objects in the wall list
+        _WallObjects.Add(GetNode<Sprite2D>("Straight"));
+        _WallObjects.Add(GetNode<Sprite2D>("Side"));
+        _WallObjects.Add(GetNode<Sprite2D>("BottomLeft"));
+        _WallObjects.Add( GetNode<Sprite2D>("BottomRight"));
+        _WallObjects.Add(GetNode<Sprite2D>("TopLeft"));
+        _WallObjects.Add(GetNode<Sprite2D>("TopRight"));
+    }
+
+    /// <summary>
+    ///  Sets the wall we want to show manually
+    /// </summary>
+    /// <param name="wallName"></param>
+    public void ShowWall(string wallName)
+    {
+        HideAllWalls();
+        GetNode<Sprite2D>(wallName).Visible = true;
+    }
+
+    /// <summary>
+    /// Gets the next wall
+    /// </summary>
+    public void CycleWallForward()
+    {
+        HideAllWalls();                         // Hide all the walls
+        _WallObjectIndex += 1;                      // Set the new index
+        
+        // Check if we need to reset the index
+        if (_WallObjectIndex >= _WallObjects.Count)
+            _WallObjectIndex = 1;
+
+        _WallObjects[_WallObjectIndex].Visible = true;                  // Show the wall
+    }
+
+    /// <summary>
+    /// Gets the previous wall
+    /// </summary>
+    public void CycleWallBackwards()
+    {
+        HideAllWalls();                 // Hide all the walls
+        _WallObjectIndex -= 1;              // Set the new index
+        
+        // Check if we need to reset the index
+        if (_WallObjectIndex < 0)
+            _WallObjectIndex = _WallObjects.Count - 1;
+
+        _WallObjects[_WallObjectIndex].Visible = true;              // Display the wall
+    }
+
+    private void HideAllWalls()
+    {
+        foreach (var wall in _WallObjects)
+            wall.Visible = false;
+    }
+}
