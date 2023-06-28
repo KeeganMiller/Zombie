dd0a6403a4c7d0fecbf712322d713747640542fa
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-28 11:24:18 +1000

    Added constructor and method to add events

--- Scripts/Controller/Timer.cs
+++ Scripts/Controller/Timer.cs
@@ -4,5 +4,22 @@ using Godot;
 
 public class Timer
 {
+    public string TimerName = "";
+    public float TimerLength;
+    private float _CurrentTime;
+    public bool _IsActive;
+    private bool _Loop;
+    private event Action _TimerCompleteActions;
+
+    public Timer(string name, float length, bool loop = false)
+    {
+        TimerName = name;
+        TimerLength = length;
+        _IsActive = true;
+        _Loop = loop;
+    }
     
+    
+
+    public void AddAction(Action e) => _TimerCompleteActions += e;
 }
