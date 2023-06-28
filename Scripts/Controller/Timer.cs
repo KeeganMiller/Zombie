ba74632075d5cf5570c0208a0c2b1aa0275a307d
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-28 11:27:58 +1000

    Added a method to update the timer, and complete the timer

--- Scripts/Controller/Timer.cs
+++ Scripts/Controller/Timer.cs
@@ -4,22 +4,40 @@ using Godot;
 
 public class Timer
 {
-    public string TimerName = "";
-    public float TimerLength;
-    private float _CurrentTime;
-    public bool _IsActive;
-    private bool _Loop;
-    private event Action _TimerCompleteActions;
+    public string TimerName = "";               // Reference to the name of the timer
+    public float TimerLength;                   // How long the timer runs for
+    private float _CurrentTime;                 // Current time of the timer
+    public bool IsActive;                       // If we should update the timer
+    private bool _Loop;                         // Will timer restart on loop
+    private event Action _TimerCompleteActions;             // actions for when timer is completed
 
     public Timer(string name, float length, bool loop = false)
     {
         TimerName = name;
         TimerLength = length;
-        _IsActive = true;
+        IsActive = true;
         _Loop = loop;
     }
-    
-    
+
+    public void Update(float delta)
+    {
+        // Make sure the timer is active
+        if (!IsActive)
+            return;
+
+        _CurrentTime += 1 * delta;              // Increment the timer
+
+        // If the timer is great than the length, timer is complete
+        if (_CurrentTime > TimerLength)
+            CompleteTimer();
+    }
+
+    private void CompleteTimer()
+    {
+        _TimerCompleteActions?.Invoke();
+        _CurrentTime = 0f;
+        IsActive = _Loop;
+    }
 
     public void AddAction(Action e) => _TimerCompleteActions += e;
 }
