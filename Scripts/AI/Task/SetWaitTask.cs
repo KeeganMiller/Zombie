fd7be314ca05e6dcaf068a31eba97ba7374e7c98
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-30 08:10:36 +1000

    Added a task to trigger waiting

---
+++ Scripts/AI/Task/SetWaitTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Godot;
+
+public class SetWaitTask : Task
+{
+    private float _WaitTime = 5.0f;
+    public SetWaitTask(BehaviorTree tree, float waitTime) : base(tree)
+    {
+        _WaitTime = waitTime;
+    }
+
+    public SetWaitTask(BehaviorTree tree, List<Task> children) : base(tree, children)
+    {
+    }
+
+    public override ETaskState RunTask(float delta)
+    {
+        if (_Tree == null)
+            return ETaskState.FAILURE;
+
+        Blackboard bb = _Tree.BlackboardRef;
+        if (bb != null)
+        {
+            bb.SetValueAsBool("IsWaiting", true);
+            bb.SetValueAsFloat("WaitTime", _WaitTime);
+            return ETaskState.SUCCESS;
+        }
+
+        return ETaskState.FAILURE;
+
+    }
+}
