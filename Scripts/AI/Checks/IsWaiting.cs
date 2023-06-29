f92379dbdf34f59f8af2ce9eb8599851e90bb4cb
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-30 07:58:01 +1000

    Create check task for waiting

---
+++ Scripts/AI/Checks/IsWaiting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System;
+using Godot;
+
+public class IsWaiting : Task
+{
+    public IsWaiting(BehaviorTree tree) : base(tree)
+    {
+    }
+
+    public IsWaiting(BehaviorTree tree, List<Task> children) : base(tree, children)
+    {
+    }
+
+    public override ETaskState RunTask(float delta)
+    {
+        if (_Tree == null)
+            return ETaskState.FAILURE;
+
+        Blackboard bb = _Tree.BlackboardRef;
+        if (bb != null)
+        {
+            if (bb.GetValueAsBool("IsWaiting"))
+                return ETaskState.SUCCESS;
+        }
+
+        return ETaskState.FAILURE;
+    }
+}
