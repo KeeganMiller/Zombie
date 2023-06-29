d4d00fe0e38855b6f3d052c2cd239575606e2e5c
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-30 07:54:07 +1000

    Added base power and resources

--- Scripts/Building/BuildingController.cs
+++ Scripts/Building/BuildingController.cs
@@ -12,24 +12,64 @@ public enum EResourceType
 
 public partial class BuildingController : Node2D
 {
+    [ExportGroup("Building Details")] 
+    [Export] private string _BuildingName;
+
+    public string BuildingName => _BuildingName;
+    
     [ExportGroup("Building Size")]
-    [Export] private int _BuildingSizeX;
-    [Export] private int _BuildingSizeY;
-    [Export] private int _DoorIndex;
+    [Export] protected int _BuildingSizeX;
+    [Export] protected int _BuildingSizeY;
+    [Export] protected int _DoorIndex;
 
     public int BuildingSizeX => _BuildingSizeX;
     public int BuildingSizeY => _BuildingSizeY;
     public int DoorIndex => _DoorIndex;
 
     [ExportGroup("Power Settings")] 
-    [Export] private bool _RequiresPower;                   // If this building requires power or not
-    [Export] private float _BasePowerUsage;               // How much this building uses in power per second
-    [Export] private float _PowerSavingModifier = 1.0f;               // Adjust how much power is saved on this room
+    [Export] protected bool _RequiresPower;                   // If this building requires power or not
+    [Export] protected float _BasePowerUsage;               // How much this building uses in power per second
+    [Export] protected float _PowerSavingModifier = 1.0f;               // Adjust how much power is saved on this room
 
     [ExportGroup("Resource Generation")] 
-    [Export] private bool _GeneratesResource = true;
-    [Export] private EResourceType _ResourceType = EResourceType.OTHER;
+    [Export] protected bool _GeneratesResource = true;                    // If this building is generating resources
+    [Export] protected EResourceType _ResourceType = EResourceType.OTHER;             // Type of resource being generated
+    [Export] protected float _ResourcesPerCollection;                      // How many resourse is collected when the room is ready
+    [Export] protected float _BaseResourceWaitTime;
+    [Export] protected float _ResourceSpeedModifier = 1.0f;               // How much faster the timer is 
+    private Timer _ResourceTimer;
 
+    public override void _Ready()
+    {
+        base._Ready();
+
+        _ResourceTimer = new Timer($"{_BuildingName} - ResourceTimer", (_BaseResourceWaitTime * _ResourceSpeedModifier),
+            false);
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        
+        // Update the resource timer
+        if(_ResourceTimer != null)
+            _ResourceTimer.Update((float)delta);
+    }
+
+    protected virtual void OnResourceComplete()
+    {
+        
+    }
+
+    public virtual void AddSettler()
+    {
+        
+    }
+
+    public void OnCollectResource()
+    {
+        
+    }
 
 
 }
