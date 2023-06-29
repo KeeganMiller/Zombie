649c9117de2fffdf979365fc43a5e68c95dd8581
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-30 08:30:34 +1000

    Created a check for the current movement state

---
+++ Scripts/AI/Checks/MoveStateCheck.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Godot;
+
+public class MovementStateCheck : Task
+{
+    private EMovementState _CheckingState;
+    public MovementStateCheck(BehaviorTree tree, EMovementState state) : base(tree)
+    {
+        _CheckingState = state;
+    }
+
+    public MovementStateCheck(BehaviorTree tree, List<Task> children) : base(tree, children)
+    {
+        
+    }
+
+    public override ETaskState RunTask(float delta)
+    {
+        if (_Tree == null)
+            return ETaskState.FAILURE;
+
+        Blackboard bb = _Tree.BlackboardRef;
+        if (bb != null)
+        {
+            EMovementState currentState = (EMovementState)bb.GetValueAsInt("MovementState");
+            if (currentState == _CheckingState)
+                return ETaskState.SUCCESS;
+        }
+
+        return ETaskState.FAILURE;
+    }
+}
