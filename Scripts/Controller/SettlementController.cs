8abdecab5fe8b23612b4111f9d46321242a2dfb6
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-30 11:21:26 +1000

    Added properties for the settlement

--- Scripts/Controller/SettlementController.cs
+++ Scripts/Controller/SettlementController.cs
@@ -4,4 +4,7 @@ using Godot;
 
 public class SettlementController : Node2D
 {
+    private List<SettlerController> _Settlers = new List<SettlerController>();              // Reference to the settlers currently in settlement
+    private int _MaxSettlers = 6;               // How many settlers 
+    private List<BuildingController> _Buildings = new List<BuildingController>();               // Reference all the buildings in the settlement
 }
