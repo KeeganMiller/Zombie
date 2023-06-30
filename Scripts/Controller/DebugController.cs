4381337d7f868ab29c627f0d2fbcd98b14807495
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-30 12:27:44 +1000

    Debug code

--- Scripts/Controller/DebugController.cs
+++ Scripts/Controller/DebugController.cs
@@ -1,6 +1,16 @@
-﻿namespace Zombie.Scripts.Controller;
+﻿using System;
+using System.Collections.Generic;
+using Godot;
 
-public class DebugController
+public partial class DebugController : Node2D
 {
-    
+    [Export] private NodePath _DebugSettler;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        SettlerController settler = GetNode<SettlerController>(_DebugSettler);
+        if (settler != null)
+            GetNode<SettlementController>("/root/SettlementController").AddSettler(settler);
+    }
 }
